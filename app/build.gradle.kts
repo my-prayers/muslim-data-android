ff190f3765b4c47ca164cd36ebd91e5ed3a55b50
Kosrat Ahmed <kosrat.d.ahmad@gmail.com>
2025-05-09 20:43:20 +0300

    build(deps): Update dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
 
     // Tests
     testImplementation("junit:junit:4.13.2")
